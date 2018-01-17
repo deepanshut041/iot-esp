43ea4ae8a0308c814468f25b32e01bf91e3a430a
deepanshut041 <deepanshut041@gmail.com>
2018-01-17 13:16:28 +0530

    made server now working on front end

--- server/index.js
+++ server/index.js
@@ -21,10 +21,16 @@ app.use(bodyParser.json());
 //Set Static Folder
 app.use(express.static(path.join(__dirname, 'client/public')))
 
+let switch_value = 0;
 
 app.get('/', (req, res) => {
-    res.send("<h1>Hello World</h1>")
+    res.json({"switch_value":switch_value})
 });
+
+app.post('/', (req, res)=>{
+    switch_value = req.body.switch_value
+    res.json({success:true, msg:"Update value successfully"});
+})
 
 server.listen(port, ()=>{
     console.log('Server is started at port : ' + port);
