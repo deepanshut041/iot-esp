e5c6107961272eed7f7fa3265e9e6668df732126
deepanshut041 <deepanshut041@gmail.com>
2018-01-19 03:00:22 +0530

    Added angular configuration in project nowworking on reactive form

--- server/src/esp/urls.py
+++ server/src/esp/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls import url
+from django.views.generic.base import TemplateView
 
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     re_path(r'^api/v1/', include('board.urls')),
-    # re_path(r'^.*', TemplateView.as_view(template_name="ang_home.html"), name='home'),
+    re_path(r'^.*', TemplateView.as_view(template_name="ang_home.html"), name='home'),
 ]
