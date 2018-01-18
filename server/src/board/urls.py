955727651697300cd042131164fa21821ec5bbcc
deepanshut041 <deepanshut041@gmail.com>
2018-01-18 20:16:38 +0530

    getting item using switch board

--- server/src/board/urls.py
+++ server/src/board/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import re_path, include, url
 
 from .views import ApplianceModelView
 urlpatterns = [
-    url(r'^board/$', ApplianceModelView.as_view(), name='appliance'),
+    url(r'^board/(?P<username>\w+)/$', ApplianceModelView.as_view(), name='appliance'),
 ]
--- server/src/board/views.py
+++ server/src/board/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import Http404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
@@ -11,3 +11,16 @@ from .serializer import ApplianceModelSerializer
 class ApplianceModelView(APIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = ApplianceModelSerializer
+
+    def get_appliance(self, pk):
+        try:
+            return Appliance.objects.filter(pk=pk)
+        except Appliance.DoesNotExist:
+            raise Http404
+
+    def get(self, request, username):
+        appliance = self.get_appliance(username)
+        appliance_serializer = ApplianceModelSerializer(appliance, many=True)
+
+        return Response(appliance_serializer.data)
+    
